a67b974d88d3369186dcdca6ecfd2e06d2099923
Bruno Zutim <bzutim@gmail.com>
2020-01-27 13:25:34 -0300

    refactor(toast): react reduc hooks

--- src/components/common/Toast.jsx
+++ src/components/common/Toast.jsx
@@ -1,70 +1,76 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+//import { bindActionCreators } from 'redux';
+import { /*connect*/ useSelector, /*useDispatch*/ } from 'react-redux';
 import styled from 'styled-components';
 
-import { setToast } from '../../redux/actions/toastActions';
+//import { setToast } from '../../redux/actions/toastActions';
 
-class Toast extends Component {
+export default function Toast() {
 
-    componentDidUpdate(){
+    const toasts = useSelector( (state) => state.toast);
+    //const dispatch = useDispatch();
 
-        window.$('.toast').toast('show')
-    }
+    useEffect( () => {
 
-    renderToasts(){
+        window.$('.toast').toast('show');
 
-        return this.props.toast.messages.map( (toast, index) => {
+    }, [toasts]);
 
-            let color = null;
+    function renderToasts(){
+
+        if(toasts.messages[0].title){
+
+            return toasts.messages.map( (toast, index) => {
+    
+                let color = null;
+            
+                switch(toast.color){
         
-            switch(toast.color){
+                    case 'success':
+                    case 'green':
+                        color = '#90EE90';
+                    break;
+        
+                    case 'warning':
+                    case 'yellow':
+                        color = '#ede858';
+                    break;
+        
+                    case 'danger':
+                    case 'red':
+                        color = '#FF6347';
+                    break;
+        
+                    default:
+                        color = toast.color;
+                }
     
-                case 'success':
-                case 'green':
-                    color = '#90EE90';
-                break;
-    
-                case 'warning':
-                case 'yellow':
-                    color = '#ede858';
-                break;
-    
-                case 'danger':
-                case 'red':
-                    color = '#FF6347';
-                break;
-    
-                default:
-                    color = toast.color;
-            }
-
-            return (
-                <DivToast key={index} className="toast" role="alert" data-delay="5000" aria-live="assertive" aria-atomic="true">
-                    <DivToastHeader className="toast-header" color={color}>
-                        <strong className="mr-auto">{toast.title || 'Mensagem'}</strong>
-                        <button type="button" className="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </DivToastHeader>
-                    <div className="toast-body">
-                        <PCenter>{toast.text}</PCenter>
-                    </div>
-                </DivToast>
-            );
-        });
+                return (
+                    <DivToast key={index} className="toast" role="alert" data-delay="5000" aria-live="assertive" aria-atomic="true">
+                        <DivToastHeader className="toast-header" color={color}>
+                            <strong className="mr-auto">{toast.title || 'Mensagem'}</strong>
+                            <button type="button" className="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </DivToastHeader>
+                        <div className="toast-body">
+                            <PCenter>{toast.text}</PCenter>
+                        </div>
+                    </DivToast>
+                );
+            });
+        }
     }
 
-    render() {
+    
+    return (
+        <DivAbsolute>
+            
+            {renderToasts()}
 
-        return (
-            <DivAbsolute>
-                
-                {this.renderToasts()}
-
-            </DivAbsolute>
-        );
-    }
+        </DivAbsolute>
+    );
+    
 }
 
 const DivAbsolute = styled.div`
@@ -90,8 +96,8 @@ const PCenter = styled.p`
 
 /////////////////////////////////////
 
-const mapStateToProps = (state) => ({ toast: state.toast });
+/*const mapStateToProps = (state) => ({ toast: state.toast });
 
 const mapDispatchToProps = (dispatch) => bindActionCreators({ setToast }, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(Toast);
+export default connect(mapStateToProps, mapDispatchToProps)(Toast);*/
